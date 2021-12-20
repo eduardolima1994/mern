360552fdfd4e2bc8d1786f58efc86d4c5f8e8f04
eduardolima1994 <camposnovospa@gmail.com>
2021-12-20 14:23:15 -0300

    CRUD Produtos

--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,23 @@ const express = require('express');
 const routes = express.Router();
 
 const Usuario = require('./controllers/usuarios.controller')
+const Produto = require('./controllers/produtos.controller')
 
+// => ROTAS USUÁRIOS
 routes.get('/', Usuario.index);
-
-// => ROTAS 
 routes.post('/api/usuarios', Usuario.create);
 routes.get('/api/usuarios', Usuario.index);
 routes.get('/api/usuarios.details/:_id', Usuario.details);
 routes.delete('/api/usuarios/:_id', Usuario.delete);
 routes.put('/api/usuarios', Usuario.update);
+
+// => ROTAS PRODUTOS
+routes.get('/', Produto.index);
+routes.post('/api/produtos', Produto.create);
+routes.get('/api/produtos', Produto.index);
+routes.get('/api/produtos.details/:_id', Produto.details);
+routes.delete('/api/produtos/:_id', Produto.delete);
+routes.put('/api/produtos', Produto.update);
 
 module.exports = routes;
 
