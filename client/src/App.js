bb337706b8750c68d99d92ef6b718da6842bbc6f
eduardolima1994 <camposnovospa@gmail.com>
2021-12-21 12:59:02 -0300

    Instalando o Material UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
+import Inicio from './pages/client/painel';
+
 export default function App() {
   return (
     <div className="App">
+      
+      {/*
       <header className="App-header">
         <h1>FRONTEND FUNCIONANDO!</h1>
       </header>
+      */}
+
+      <Inicio />
     </div>
   );
 }
