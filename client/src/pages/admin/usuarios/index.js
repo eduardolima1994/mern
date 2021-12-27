495bfa75b857d9d9dc745249a354b3cd2b3c2beb
eduardolima1994 <eduardolima.tecnico@gmail.com>
2021-12-27 11:37:15 -0300

    Listagem de Usuários

--- client/src/pages/admin/usuarios/index.js
+++ client/src/pages/admin/usuarios/index.js
@@ -1,9 +1,112 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import MenuAdmin from '../../../components/menu-admin';
+import Footer from '../../../components/footer-admin';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+
+import api from '../../../services/api'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  title: {
+    flexGrow: 1,
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+}));
 
 export default function UsuariosListagem() {
+
+  const classes = useStyles();
+
+  const [usuarios, setUsuarios] = useState([]);
+
+  useEffect(()=>{
+    async function loadUsuarios(){
+      const response = await api.get("/api/usuarios");
+      setUsuarios(response.data);
+    };
+    loadUsuarios();
+  }, []);
+
   return (
-    <div>
-     <h1>Listagem de Usuários</h1>
+    <div className={classes.root}>
+      <MenuAdmin title={'USUÁRIOS'} />
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        <Container maxWidth="lg" className={classes.container}>
+          <Grid container spacing={3}>
+            <Grid item sm={12}>
+              <Paper className={classes.paper}>
+                <h2>Listagem de Usuários</h2>
+                <Grid container spacing={3}>
+                  <Grid item xs={12} sm={12}>
+                    <TableContainer component={Paper}>
+                      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+                        <TableHead>
+                          <TableRow>
+                            <TableCell>Nome</TableCell>
+                            <TableCell align="center">Email</TableCell>
+                            <TableCell align="center">Tipo</TableCell>
+                            <TableCell align="center">Data de Cadastro</TableCell>
+                            <TableCell align="right">Opções</TableCell>
+                          </TableRow>
+                        </TableHead>
+                        <TableBody>
+                          
+                          {usuarios.map((row) => (
+                            <TableRow
+                              key={row._id}
+                              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                              <TableCell component="th" scope="row">
+                                {row.nome_usuario}
+                              </TableCell>
+                              <TableCell>{row.email_usuario}</TableCell>
+                              <TableCell>{row.tipo_usuario === 1 ? 'Administrador' : 'Usuário Comum'}</TableCell>
+                              <TableCell>{row.createdAt}</TableCell>
+                              <TableCell align="right">Botões</TableCell>
+                            </TableRow>
+                          ))}
+                          
+                        </TableBody>
+                      </Table>
+                    </TableContainer>
+                  </Grid>
+                </Grid>
+              </Paper>
+            </Grid>
+          </Grid>
+          <Box pt={4}>
+            <Footer />
+          </Box>
+        </Container>
+      </main>
     </div>
   );
 }
