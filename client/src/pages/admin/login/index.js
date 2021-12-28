fd6b3642f04a90de6d39eb0d2c50c1558e80f11d
eduardolima1994 <eduardolima.tecnico@gmail.com>
2021-12-28 15:57:06 -0300

    Organização do Código

--- client/src/pages/admin/login/index.js
+++ client/src/pages/admin/login/index.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {useState} from 'react';
+import { useState } from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -13,107 +13,107 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import api from '../../../services/api'
 
-import {login, setIdUsuario, setNomeUsuario} from '../../../services/auth'
+import { login, setIdUsuario, setNomeUsuario } from '../../../services/auth'
 
 function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-        {'Copyrigth © '}
-        <Link color="inherit" href="javascript:;">
-            Sistema MERN 
-        </Link>
-        {' ' + new Date().getFullYear()}
-    </Typography>
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyrigth © '}
+            <Link color="inherit" href="javascript:;">
+                Sistema MERN
+            </Link>
+            {' ' + new Date().getFullYear()}
+        </Typography>
 
-    
-  );
+
+    );
 }
 
 const theme = createTheme();
 
 export default function SignIn() {
 
-    const [email,setEmail] = useState('');
-    const [senha,setSenha] = useState('');
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
 
-    async function handleSubmit(){
-        await api.post('/api/usuarios/login', {email,senha})
-        .then(res => {
-            if(res.status === 200){
-                if(res.data.status === 1){
-                    
-                    login(res.data.token);
-                    setIdUsuario(res.data.id_client);
-                    setNomeUsuario(res.data.user_name);
+    async function handleSubmit() {
+        await api.post('/api/usuarios/login', { email, senha })
+            .then(res => {
+                if (res.status === 200) {
+                    if (res.data.status === 1) {
 
-                    window.location.href='/admin';
-                }else if(res.data.status === 2){
-                    alert('Atenção: '+res.data.error);
+                        login(res.data.token);
+                        setIdUsuario(res.data.id_client);
+                        setNomeUsuario(res.data.user_name);
+
+                        window.location.href = '/admin';
+                    } else if (res.data.status === 2) {
+                        alert('Atenção: ' + res.data.error);
+                    }
+                } else {
+                    alert('Erro no servidor!');
                 }
-            }else{
-                alert('Erro no servidor!');
-            }
-        })
+            })
     }
 
-  return (
-    
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Login
-          </Typography>
-          <Box component="form" noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Digite seu email"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              value={email}
-              onChange={(e)=>setEmail(e.target.value)}
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Digite sua senha"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              value={senha}
-              onChange={(e)=>setSenha(e.target.value)}
-            />
-            
-            <Button
-              
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              onClick={handleSubmit}
+    return (
+
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
             >
-              ENTRAR
-            </Button>
-            
-          </Box>
-        </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
-    
-  );
+                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Login
+                </Typography>
+                <Box component="form" noValidate sx={{ mt: 1 }}>
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Digite seu email"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Digite sua senha"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                        value={senha}
+                        onChange={(e) => setSenha(e.target.value)}
+                    />
+
+                    <Button
+
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                        onClick={handleSubmit}
+                    >
+                        ENTRAR
+                    </Button>
+
+                </Box>
+            </Box>
+            <Copyright sx={{ mt: 8, mb: 4 }} />
+        </Container>
+
+    );
 }
