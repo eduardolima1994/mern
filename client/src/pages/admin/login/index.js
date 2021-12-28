14deaae98aa0af2ff0bf5991df70ef5b79a1a737
eduardolima1994 <eduardolima.tecnico@gmail.com>
2021-12-28 14:03:27 -0300

    Frontend Login

--- client/src/pages/admin/login/index.js
+++ client/src/pages/admin/login/index.js
@@ -1,9 +1,99 @@
-import React from 'react';
+import * as React from 'react';
+import {useState} from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-export default function Login(){
-    return(
-        <>
-            <h1>Tela de Login</h1>
-        </>
-    );
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+        {'Copyrigth © '}
+        <Link color="inherit" href="javascript:;">
+            Sistema MERN 
+        </Link>
+        {' ' + new Date().getFullYear()}
+    </Typography>
+
+    
+  );
+}
+
+const theme = createTheme();
+
+export default function SignIn() {
+
+    const [email,setEmail] = useState('');
+    const [senha,setSenha] = useState('');
+
+    async function handleSubmit(){
+        alert('Autenticar: ' + email);
+    }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Login
+          </Typography>
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Digite seu email"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              value={email}
+              onChange={(e)=>setEmail(e.target.value)}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Digite sua senha"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={senha}
+              onChange={(e)=>setSenha(e.target.value)}
+            />
+            
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={handleSubmit}
+            >
+              ENTRAR
+            </Button>
+            
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
 }
