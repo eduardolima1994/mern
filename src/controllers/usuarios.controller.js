a4ca3e6935040d7c8edcf95530f95f344c7ae7ec
eduardolima1994 <eduardolima.tecnico@gmail.com>
2021-12-28 15:41:51 -0300

    Mensagens de Autenticação

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -51,14 +51,23 @@ module.exports = {
                 console.log(err);
                 res.status(200).json({erro:"Erro no servidor! Por favor, tente novamente!"});
             }else if(!user){
-                res.status(200).json({status:2, error:"Email ou senha não conferem!"});
+                res.status(200).json({status:2, error:"Email não encontrado no banco de dados!"});
             }else{
-                const payload = { email };
-                const token = jwt.sign(payload, secret, {
-                    expiresIn: '24h',
-                })
-                res.cookie('token', token, {httpOnly: true});
-                res.status(200).json({ status:1, auth:true, token:token, id_cliente: user._id, user_name: user.nome_usuario })
+                user.isCorrectPassword(senha, async function(err,same){
+                    if(err){
+                        res.status(200).json({error:"Erro no servidor! Por favor tente novamente!"});
+                    }else if(!same){
+                        res.status(200).json({status:2, error:"A senha não confere!"});
+                    }else{
+                        const payload = { email };
+                        const token = jwt.sign(payload, secret, {
+                            expiresIn: '24h',
+                        })
+                        res.cookie('token', token, {httpOnly: true});
+                        res.status(200).json({ status:1, auth:true, token:token, id_cliente: user._id, user_name: user.nome_usuario })
+                    }
+                });
+                
             }
         });
     },
